c49b3a471444dbac37131eb83218fdd684ce0e56
Thomas Lipp <4180596+thomats@users.noreply.github.com>
2023-03-06 13:17:02 +0100

    Fix: add -Recursive to  forEach loop

--- SSP_CheckProjectProgress_V1.0.ps1
+++ SSP_CheckProjectProgress_V1.0.ps1
@@ -1,33 +1,40 @@
 # Define Global Variables
-# Name: SSP_CheckProjectProgress_V1.0.ps1
-# Version: 1.0
-# Autor: Thomas Lipp | CSF AG
+# Name: SSP_CheckProjectProgress_V2.0.ps1
+# Version: 2.0
+# Autor: Thomas Lipp, Michel Barmasse | CSF AG
 
 # GLOBAL VARIABLES
 $rootDirectory = "\\SSPFIL1\Projektdaten\"
 $date = Get-Date
 $time = $date.AddDays(-30)
+$reportOutput = "D:\Daten\Sekretariat\06 IT\_Rapports\Projektreport.csv"
 
 
 #SCRIPT START
-Write-Host "Script gestartet - Projektaenderung werden geprueft..."
+Write-Host "Script wurde gestartet"
+
+# Löschen der vorhandenen Reportdatei
+Write-Host "Veraltete Reportdatei wird gelöscht."
+Remove-Item -Path $reportOutput
 
 # Auslesen aller Ordner mit dem Namen 02_CAD zwei Hierarchiestufen vom Root Verzeichnis
+Write-Host "Projektänderungen werden ausgelesen."
 $projectFolders = Get-ChildItem -Path $rootDirectory -filter "*02_CAD*" -depth 2 | % { $_.FullName }
 
-Write-Host -ForegroundColor Green "Zielordner wurden erfolgreich ausgelesen"
+
 
 # Auslesen der Zielordner im Ordner 02_CAD
 foreach ($projectFolder in $projectFolders) {
-    $targetFolders = Get-ChildItem -Path $projectFolder | Where-Object { $_.Name -Like "*02_K*" -or $_.Name -Like "*03_K*" -or $_.name -Like "05_Schema" }
-    # Zusammensetzen der Teilpfade zu einem vollständigen Pfad
-    foreach ($targetFolder in $targetFolders) {
+    $targetFolders = Get-ChildItem -Path $projectFolder -Recurse | Where-Object { $_.Name -Like "02_Kälte" -or $_.Name -Like "03_Kühlraum" -or $_.name -Like "05_Schema" }
+    
+    # Zusammensetzen der Teilpfade zu einem vollstÃ¤ndigen Pfad
+ foreach ($targetFolder in $targetFolders) {
         $fullPath = join-path -path $projectFolder -childpath $targetFolder
 
         # Auslesen aller PDF Files neuer als 30 Tage & Export in CSV-Datei
         foreach ($path in $fullPath) {
-            $overdueFiles = Get-ChildItem -Path $fullPath | Where-Object { ($_.LastWriteTime -gt $time) -and ($_.extension -like ".pdf") } | out-file -append C:\csfinstall\csf.csv | Export-Csv -Path  C:\csfinstall\output.csv -NoTypeInformation -Append -Verbose -Delimiter "," 
-            Write-host $overdueFiles
+            $overdueFiles = Get-ChildItem -Path $fullPath -Recurse | Where-Object { ($_.LastWriteTime -gt $time) -and ($_.extension -like ".pdf") } | out-file -append $reportOutput 
+        
         }
 
     }
